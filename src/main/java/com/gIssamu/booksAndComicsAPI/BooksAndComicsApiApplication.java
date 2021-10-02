c86ccb2bb97d3b975168080cc029d807fee545f2
GabsOda <biel.oda@gmail.com>
2021-10-02 13:48:29 -0300

    first commit

---
+++ src/main/java/com/gIssamu/booksAndComicsAPI/BooksAndComicsApiApplication.java
@@ -0,0 +1,13 @@
+package com.gIssamu.booksAndComicsAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksAndComicsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooksAndComicsApiApplication.class, args);
+	}
+
+}
